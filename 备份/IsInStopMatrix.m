1f564faf9f9dd4cc3115dcc14c835aaa6b31eadf
nameVal <1415082459@qq.com>
2019-09-24 10:48:39 +0800

    092411

--- 备份/IsInStopMatrix.m
+++
@@ -1,9 +0,0 @@
-function result = IsInStopMatrix(stopMatrix,location)
-[M,N] = size(stopMatrix);
-result = 0;
-for i = 1 : M
-    if stopMatrix(M,:) == location
-       result = 1;
-       break 
-    end
-end
--- 备份/test.m
+++
@@ -1,8 +0,0 @@
-% ax+by+cz+d=0
-% y=linspace(-4,4,40);
-x = -100:0.25:100;
-y = x;
-[X,Y] = meshgrid(x,y);
-Z=1 - X - Y;
-mesh(Z,X,Y) 
-surf(Z,X,Y)
